548d1c39f23835e905d36f0827f6eb48d74d13f3
Zueon <maineon0701@gmail.com>
2022-10-07 15:13:23 +0900

    boardMapper crud

--- src/main/java/com/board/mapper/BoardMapper.java
+++ src/main/java/com/board/mapper/BoardMapper.java
@@ -8,4 +8,13 @@ import java.util.List;
 public interface BoardMapper {
 //    @Select ( "select * from tbl_board where bno > 0")
     public List<BoardVO> getList();
+
+    public void insert(BoardVO board);          // insert만 처리
+    public void insertSelectKey (BoardVO board);// insert 처리 후 생성된 pk 값 필요 시
+
+    public BoardVO getBoard(long bno);
+
+    public int deleteBoard(long bno);
+
+    public int update(BoardVO board);
 }
--- src/test/java/com/board/mapper/BoardMapperTests.java
+++ src/test/java/com/board/mapper/BoardMapperTests.java
@@ -1,5 +1,6 @@
 package com.board.mapper;
 
+import com.board.domain.BoardVO;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 import org.junit.Test;
@@ -15,8 +16,43 @@ public class BoardMapperTests {
     @Setter(onMethod_ = {@Autowired})
     private BoardMapper mapper;
 
+
     @Test
     public void getListTest(){
         mapper.getList().forEach(board -> log.info(board));
+    }
+
+    @Test
+    public void insertTest(){
+        BoardVO board = new BoardVO();
+        board.setTitle("test title1");
+        board.setContent("testContent");
+        board.setWriter("testWriter");
+        mapper.insert(board);
+
+        log.info(board);
+    }
+
+    @Test
+    public void insertSelectKeyTest(){
+        BoardVO board = new BoardVO();
+        board.setTitle("test title2");
+        board.setContent("testContent2");
+        board.setWriter("testWriter2");
+        mapper.insertSelectKey(board);
+        log.info(board);
+    }
+
+    @Test
+    public void getBoardTest(){
+        BoardVO board = mapper.getBoard(5l);
+        log.info(board);
+
+    }
+
+    @Test
+    public void deleteTest(){
+        int deletedcnt = mapper.deleteBoard(5l);
+        log.info(deletedcnt);
     }
 }
