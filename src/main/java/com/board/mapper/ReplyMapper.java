6c9498967a38787615af91e9901fcf112ece9300
Zueon <maineon0701@gmail.com>
2022-10-11 19:13:50 +0900

    reply mapper

--- src/main/java/com/board/mapper/ReplyMapper.java
+++ src/main/java/com/board/mapper/ReplyMapper.java
@@ -1,7 +1,11 @@
 package com.board.mapper;
 
 
+import com.board.domain.Criteria;
 import com.board.domain.ReplyVO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface ReplyMapper {
     // 외래키를 사용하여 답글을 등록한다.
@@ -13,4 +17,7 @@ public interface ReplyMapper {
     public int delete(Long rno);
 
     public int update(ReplyVO replyVO);
+
+    // xml로 처리할 때 지정된 두 개의 파라미터를 모두 사용할 수 있다
+    public List<ReplyVO> getListWithPaging(@Param("cri")Criteria cri, @Param("bno")Long bno);
 }
